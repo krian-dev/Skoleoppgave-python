197731a70dbc278f99e56ce37959fe1d8046102e
Andreas Skoglund <atg@xinity.co>
2023-03-13 16:41:57 +0100

    Updating ancient code for educational reasons

--- Client Stream.py
+++ Client Stream.py
@@ -4,10 +4,10 @@ from threading import Thread
 mySocket = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
 mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 print("Connecting...")
-#10.35.36.164
+
 try:
-	print(mySocket.connect(("192.168.0.108",27000)))
-except socket.error, e:
+	print(mySocket.connect(("localhost",27000)))
+except socket.error as e:
 	print(e)
 	raw_input()
 print("Connected!")
@@ -19,25 +19,26 @@ class listen(Thread):
 	
 	def run(self):
 		while alive:
-			recv=mySocket.recvfrom(100)
+			recv=mySocket.recv(100).decode("ascii")
 			if recv=="PING":
 				print("Ping recived, responding...")
-				mySocket.send("PING")
+				mySocket.send(b"PING")
 			else:
 				print(recv)
 		if not alive:
 			print("Listen is dead")
+
 TListen=listen()
 TListen.start()
 
 alive=True
 while alive:
-	Type=raw_input("<<")
+	Type=input(">> ")
 	print(Type)
 	if Type=="exit":
 		TListen.alive=False
 		mySocket.close()
 		alive=False
 	else:
-		mySocket.send(Type)
+		mySocket.send(bytes(Type, "ascii"))
 
